9be23b1f6df6e28c3e7e71e51a277bbc65809eb0
Piotr Oniszczuk <warpme@o2.pl>
2015-02-01 13:52:48 +0100

    Update checks for Nvidia 340.76

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -197,8 +197,8 @@ mm-all:
 		exit 1 ; \
 	fi
 	@echo "    mm_NVIDIA_VERSION"
-	@if [ ! "$(mm_NVIDIA_VERSION)" = "340.58"    ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "340.65"    ] ; then \
+	@if [ ! "$(mm_NVIDIA_VERSION)" = "340.65"    ] && \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "340.76"    ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
 	fi
