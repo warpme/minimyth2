d7f81a5b6c8eacf49b7fa504d398d34063ec0aaf
Piotr Oniszczuk <warpme@o2.pl>
2015-06-15 10:52:27 +0200

    Update index.html, changelog. Bump MinimMyt2 ver.to 7.6.13 cd /home/piotro/.minimyth

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -13,7 +13,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 7.6.11.ge3656ea
+mm_VERSION_MINIMYTH ?= 7.6.13.g99c61cc
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
