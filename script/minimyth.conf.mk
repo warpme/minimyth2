7c561fd5d1747919a4d8278dccbdb4906a3d35be
Piotr Oniszczuk <warpme@o2.pl>
2017-01-26 14:52:22 +0100

    Bump MiniMyth2 ver to 8.6.5

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -13,7 +13,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 8.6.2.r259
+mm_VERSION_MINIMYTH ?= 8.6.5.r289
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
