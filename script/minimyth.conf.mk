5cd5a60150e259bba64df0c17adf53cdda2da648
Piotr Oniszczuk <warpme@o2.pl>
2017-08-08 15:22:08 +0200

    Bump MiniMyth2 to v8.11.0

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -16,7 +16,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 8.10.4.r3
+mm_VERSION_MINIMYTH ?= 8.11.0.r15
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
