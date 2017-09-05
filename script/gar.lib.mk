3e00abb0f5ebe88694877864ac33f0b586e6c04a
Piotr Oniszczuk <warpme@o2.pl>
2017-09-05 11:46:19 +0200

    Add support for xz based .deb extracting

--- script/gar.lib.mk
+++ script/gar.lib.mk
@@ -167,7 +167,7 @@ cp-extract-%:
 deb-bin-extract-%:
 	@echo " ==> Extracting $(DOWNLOADDIR)/$*"
 	@ar x $(DOWNLOADDIR)/$*
-	@rm debian-binary && mv *.tar.*z $(DOWNLOADDIR) && mkdir $(WORKSRC) && tar -xJ -C $(WORKSRC) -f $(DOWNLOADDIR)/data.tar.xz
+	@rm debian-binary && mv *.tar.*z $(DOWNLOADDIR) && mkdir $(WORKSRC) && tar -x -C $(WORKSRC) -f $(DOWNLOADDIR)/data.tar.*
 	@$(MAKECOOKIE)
 
 ### EXTRACT FILE TYPE MAPPINGS ###
