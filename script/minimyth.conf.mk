b9d1ff566e05e5b0f9e2668976ffb69624e81a93
Piotr Oniszczuk <warpme@o2.pl>
2017-10-15 12:10:55 +0200

    Bump mm2 ver.to 8.13.1

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -16,7 +16,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 8.13.0.r66
+mm_VERSION_MINIMYTH ?= 8.13.1.r229
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
