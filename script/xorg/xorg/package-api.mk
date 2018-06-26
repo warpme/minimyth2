d0a3b2e84f7fdfb73a138732a58850c9a9aa2ad0
Piotr Oniszczuk <warpme@o2.pl>
2018-06-26 11:06:43 +0200

    Make xf86-video-{ati,intel,vmware} compile conditional in mm_GRAPHICS; Cleanup Xorg download MASTER_SITES

--- script/xorg/xorg/package-api.mk
+++ script/xorg/xorg/package-api.mk
@@ -1,13 +1,8 @@
 XORG_VERSION      = 7.3
 XORG_MASTER_SITES = \
-	$(foreach                                                           \
-		dir,                                                        \
-		app data doc driver everything font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/individual/$(dir)/     \
-	) \
-	$(foreach                                                           \
-		dir,                                                        \
-		app data doc driver everything font lib proto util xserver, \
-	        http://xorg.freedesktop.org/releases/X11R7.3/src/$(dir)/    \
+	$(foreach                                                               \
+		dir,                                                            \
+		app data doc driver everything font lib proto xcb util xserver, \
+	        https://xorg.freedesktop.org/releases/individual/$(dir)/        \
 	) \
 	http://xcb.freedesktop.org/dist/
