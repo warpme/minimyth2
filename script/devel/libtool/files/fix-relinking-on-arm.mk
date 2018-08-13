8406a4c0fb117139cac5459bd3dcf3a23e73d3c5
Piotr Oniszczuk <warpme@o2.pl>
2018-08-13 22:21:51 +0200

    Add patches in libtool needed for ARM cross-compile

--- script/devel/libtool/files/fix-relinking-on-arm.mk
+++
@@ -1,9 +0,0 @@
-
-post-configure:
-ifeq ($(DESTIMG),main)
-ifeq ($(GARCH_FAMILY),arm)
-	@echo "Fixing libtool relinking on ARM platform"
-	@sed -e "s%^libdir='.*%libdir='$(main_DESTDIR)$(main_libdir)'\"%" -i $(WORKSRC)/libtool
-endif
-endif
-	@$(MAKECOOKIE)
