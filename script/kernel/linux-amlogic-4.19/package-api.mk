e957f67046bde843320731eeca32eafb767fe081
Piotr Oniszczuk <warpme@o2.pl>
2018-11-20 08:04:08 +0100

    Update amlogic-4.19 kernel

--- script/kernel/linux-amlogic-4.19/package-api.mk
+++ script/kernel/linux-amlogic-4.19/package-api.mk
@@ -1,16 +1,53 @@
 
-# Use this for Elyotne kernel
-# GITHASH             = 30a7d3fe02166fa2240860fb37756a6033bb01fe
+#--Use this for mainline kernel-------------------------------
+#GITHASH             = 84df9525b0c27f3ebc2ebb1864fa62a97fdedb7d
+#LINUX_MAJOR_VERSION = 4
+#LINUX_MINOR_VERSION = 19
+#LINUX_TEENY_VERSION = 0
+#LINUX_EXTRA_VERSION = 
+#-------------------------------------------------------------
+
+#--Use this for khadas kernel---------------------------------
+# GITHASH             = b0ee9f50b8030b8dd9b26eb7b5cdef5bdbda4f35
+# LINUX_MAJOR_VERSION = 4
+# LINUX_MINOR_VERSION = 19
+# LINUX_TEENY_VERSION = 0
+# LINUX_EXTRA_VERSION = 
+#-------------------------------------------------------------
+
+#-- Use this for Elyotna kernel-------------------------------
+# GITHASH             = c8f093d221aa4f91c372b880ff4d1b8c7dc78664
+# LINUX_MAJOR_VERSION = 4
+# LINUX_MINOR_VERSION = 19
+# LINUX_TEENY_VERSION = 0
+# LINUX_EXTRA_VERSION = 
+#-------------------------------------------------------------
+
+#--Use this for balbes150 kernel------------------------------
+# GITHASH             = 2b66eefe06f198f98668871fb0e3dc9007cfd25d
 # LINUX_MAJOR_VERSION = 4
 # LINUX_MINOR_VERSION = 19
 # LINUX_TEENY_VERSION = 0
 # LINUX_EXTRA_VERSION = -rc7
+#-------------------------------------------------------------
 
-# Use this for official kernel git for MaLogic (khilman)
+#--Use this for yuq lima kernel-------------------------------
+GITHASH             = linux-lima-4.19-rc4
 LINUX_MAJOR_VERSION = 4
 LINUX_MINOR_VERSION = 19
 LINUX_TEENY_VERSION = 0
-LINUX_EXTRA_VERSION = 
+LINUX_EXTRA_VERSION = -rc4
+#-------------------------------------------------------------
+
+
+#--Use this for official kernel git for AMLogic (khilman)-----
+# LINUX_MAJOR_VERSION = 4
+# LINUX_MINOR_VERSION = 19
+# LINUX_TEENY_VERSION = 0
+# LINUX_EXTRA_VERSION = 
+#-------------------------------------------------------------
+
+
 
 LINUX_VERSION      = $(LINUX_MAJOR_VERSION).$(LINUX_MINOR_VERSION)$(if $(LINUX_TEENY_VERSION),.$(LINUX_TEENY_VERSION))$(LINUX_EXTRA_VERSION)
 LINUX_FULL_VERSION = $(LINUX_MAJOR_VERSION).$(LINUX_MINOR_VERSION)$(if $(LINUX_TEENY_VERSION),.$(LINUX_TEENY_VERSION),.0)$(LINUX_EXTRA_VERSION)
