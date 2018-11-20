78cbbcc141a0b476788e577c178df0d4d1780050
Piotr Oniszczuk <warpme@o2.pl>
2018-11-20 10:00:19 +0100

    Add nvidia 410.78 part2

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -192,8 +192,8 @@ mm-all:
 		exit 1 ; \
 	fi
 	@echo "    mm_NVIDIA_VERSION"
-	@if [ ! "$(mm_NVIDIA_VERSION)" = "396.54"    ] && \
-	    [ ! "$(mm_NVIDIA_VERSION)" = "410.66"    ] && \
+	@if [ ! "$(mm_NVIDIA_VERSION)" = "410.66"    ] && \
+	    [ ! "$(mm_NVIDIA_VERSION)" = "410.78"    ] && \
 	    [ ! "$(mm_NVIDIA_VERSION)" = "390.50"    ] ; then \
 		echo "error: mm_NVIDIA_VERSION=\"$(mm_NVIDIA_VERSION)\" is an invalid value." ; \
 		exit 1 ; \
