d90088268354a09432501cc5af6070bfc722e7ce
Piotr Oniszczuk <warpme@o2.pl>
2018-12-17 20:40:44 +0100

    Update makefile for kickstat-oe; switch SD card creation to fakeroot env so root password is not needed

--- script/utils/kickstart-oe/files/create-image.sh
+++ script/utils/kickstart-oe/files/create-image.sh
@@ -1,28 +1,39 @@
 
-mm_conf_file="/home/piotro/minimyth-dev/minimyth.conf.mk"
-upload_addr="piotro@192.168.1.9"
-boot_files_loc="/home/piotro/ABS/mythtv-pxe_image/src/boot"
-rootfs_archive="/home/piotro/ABS/mythtv-pxe_image/src/rootfs.tar.bz2"
+mm_home="/home/piotro/minimyth-dev"
+use_minimyth_python3="no"
 
 #--------------------------------------------------------------------------------------
+
+mm_conf_file="${mm_home}/minimyth.conf.mk"
 
 board=$1
 
 if [ x${board} = "x" ] ; then
-board=`  grep "^mm_BOARD_TYPE "       ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
+    board=`  grep "^mm_BOARD_TYPE "       ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 fi
-
-base_dir=${PWD}
-
-export BUILDDIR=${base_dir}
-export BBPATH=${base_dir}
-export PATH=${base_dir}/bitbake/bin:$PATH
-export PYTHONPATH=${base_dir}/bitbake/lib:${base_dir}/lib:$PYTHONPATH
-export PYTHON=/home/piotro/minimyth-dev/images/build/usr/bin/python3
 
 branch=` grep "^mm_MYTH_VERSION "     ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 version=`grep "^mm_VERSION_MINIMYTH " ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 arch=`   grep "^mm_GARCH "            ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
+
+base_dir=${mm_home}/images/build/usr/bin/kickstart
+mm_build_dir=${mm_home}/script/meta/minimyth/work/main.d/minimyth-master-${version}/build
+root_files_loc=${mm_build_dir}/stage/image/rootfs
+boot_files_loc=${mm_build_dir}/stage/boot
+
+export BUILDDIR=${mm_build_dir}/stage
+export BBPATH=${base_dir}
+export PATH=${base_dir}/bitbake/bin:${PATH}
+export PYTHONPATH=${base_dir}/bitbake/lib:${PYTHONPATH}
+
+if [ x${use_minimyth_python3} = "xyes" ] ; then 
+    export PATH=/home/piotro/minimyth-dev/images/build/bin:/home/piotro/minimyth-dev/images/build/usr/bin:${PATH}
+    export PYTHONPATH=/home/piotro/minimyth-dev/images/build/usr/lib/python3.7:${PYTHONPATH}
+    export PYTHON=/home/piotro/minimyth-dev/images/build/usr/bin/python3
+    export LD_LIBRARY_PATH=/home/piotro/minimyth-dev/images/build/usr/lib
+else
+    export PYTHON=/usr/bin/python3
+fi
 
 echo " "
 echo "---- Starting to create SD image... ----"
@@ -31,35 +42,39 @@ echo "  branch  : ${branch}"
 echo "  version : ${version}"
 echo "  arch    : ${arch}"
 echo "  board   : ${board}"
+echo "  boot    : ${boot_files_loc}"
+echo "  rootfs  : ${root_files_loc}"
 echo " "
 
 echo "--> Setting right (${board}) config file..."
 cp -f ${base_dir}/conf/multiconfig/${board}.conf ${base_dir}/conf/multiconfig/default.conf
-echo " "
 
-echo "  Creating SD image rootfs content requires root priviliges."
-echo "  Please provide root password..."
-echo " "
+echo '--> Preparing for building SD Image...'
+cd ${BUILDDIR}
+rm -f  ${BUILDDIR}/*.log
+rm -f  ${BUILDDIR}/*.direct
+rm -f  ${BUILDDIR}/*.vfat
+rm -f  ${BUILDDIR}/*.ext4
+rm -rf ${root_files_loc}/../pseudo*
+mkdir -p ${mm_build_dir}/stage/tmp/sysroots-components/x86_64/pseudo-native/usr/bin
+ln -srf ${base_dir}/native-bins/usr/bin/pseudo ${mm_build_dir}/stage/tmp/sysroots-components/x86_64/pseudo-native/usr/bin/pseudo
 
-su -c " \
-cd ${BUILDDIR}; \
-rm -f ./*.log; \
-rm -f ./*.direct; \
-rm -f ./*.vfat; \
-rm -f ./*.ext4; \
-rm -f ./*.tar.bz2; \
-rm -rf ./rootfs*; \
-rm -rf ./pseudo*; \
-echo '--> Unpacking rootfs...'; \
-tar -jxf ${rootfs_archive} -C ./; \
-echo '--> Building SD Image...'; \
-${base_dir}/scripts/wic create MiniMyth2.wks --bootimg-dir=${boot_files_loc} --kernel-dir=${boot_files_loc} --rootfs-dir=${base_dir}/rootfs --native-sysroot='/'; \
-rename MiniMyth2-*.direct MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.img *; \
-echo '--> Removing working files...'; \
-rm -rf ./rootfs*; \
-rm -rf ./pseudo*; \
-echo '--> Packaging image...'; \
-tar cjvf MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2 ./*.img; \
-rm -f ./*.img; \
-echo '--> Image creation done!';
+echo '--> Entering fakeroot enviroment...'
+fakeroot -i ${mm_build_dir}/stage/image/rootfs.fakeroot sh -c " \
+${PYTHON} ${base_dir}/scripts/wic create ${base_dir}/MiniMyth2.wks \
+--bootimg-dir=${boot_files_loc} \
+--kernel-dir=${boot_files_loc} \
+--rootfs-dir=${root_files_loc} \
+--native-sysroot=${base_dir}/native-bins \
 "
+echo '--> Removing working files...'
+rm -rf ${root_files_loc}/../pseudo*
+
+echo '--> Packaging image...'
+rename MiniMyth2-*.direct MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.img *
+tar cjvf MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2 ./*.img
+rm -f ./*.img
+
+echo '--> Image creation done!'
+
+exit 0
--- script/utils/kickstart-oe/files/upload-burn.sh
+++ script/utils/kickstart-oe/files/upload-burn.sh
@@ -1,19 +1,29 @@
 
-mm_conf_file="/home/piotro/minimyth-dev/minimyth.conf.mk"
+#--------------------------------------------------------------------------------------
+
+mm_home="/home/piotro/minimyth-dev"
 upload_addr="piotro@192.168.1.9"
-base_dir="/home/piotro/ABS/SD-image-builder"
 
 #--------------------------------------------------------------------------------------
+
+
+
+
+
+mm_conf_file="${mm_home}/minimyth.conf.mk"
 
 board=$1
 
 if [ x${board} = "x" ] ; then
-board=`  grep "^mm_BOARD_TYPE "       ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
+    board=`  grep "^mm_BOARD_TYPE "       ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 fi
 
 branch=` grep "^mm_MYTH_VERSION "     ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 version=`grep "^mm_VERSION_MINIMYTH " ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 arch=`   grep "^mm_GARCH "            ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
+
+base_dir=${mm_home}/images/build/usr/bin/kickstart
+mm_build_dir=${mm_home}/script/meta/minimyth/work/main.d/minimyth-master-${version}/build
 
 echo " "
 echo "---- Uploading SD image... ----"
@@ -28,7 +38,7 @@ echo " "
 echo "--> Uploading file to destimation (${upload_addr})..."
 echo "(This may require account password on target machine)"
 echo " "
-scp -c none ./MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2 ${upload_addr}:Desktop/MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2
+scp -c none ${mm_build_dir}/stage/MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2 ${upload_addr}:Desktop/MiniMyth2-${arch}-${branch}-${version}-${board}-SD-Image.tar.bz2
 echo '--> Image upload done!';
 
 echo " "
