a01d58054007db15f2308842f9a243e295b719cf
Piotr Oniszczuk <warpme@o2.pl>
2019-01-06 16:32:54 +0100

    Bump mm2 ver.to 9.11.0

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -16,7 +16,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk,        $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 9.11.0.r1117
+mm_VERSION_MINIMYTH ?= 9.11.0.r1119
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
