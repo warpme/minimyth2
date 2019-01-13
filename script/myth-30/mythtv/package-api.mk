9351af55360df72dd423947a34915295bd832499
Piotr Oniszczuk <warpme@o2.pl>
2019-01-13 14:37:01 +0100

    Add mythtv-30 as ver.to compile

---
+++ script/myth-30/mythtv/package-api.mk
@@ -0,0 +1,26 @@
+MYTHTV_GARVERSION_SHORT = 30
+MYTHTV_SVN_VERSION = 20190113-g8b667bb
+MYTHTV_GIT_VERSION = v30-Pre-1125-g8b667bb
+
+MYTHTV_VERSION = $(MYTHTV_GARVERSION_SHORT)-$(MYTHTV_SVN_VERSION)
+
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_CONFIGURE_ENV = \
+	PYTHONXCPREFIX=$(DESTDIR)$(prefix)
+MYTHTV_BUILD_ENV     =
+MYTHTV_INSTALL_ENV   = \
+	INSTALL_ROOT="/" \
+
+MYTHTV_PLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(DESTDIR)$(prefix)" \
+	--sysroot="$(DESTDIR)$(rootdir)" \
+	--qmake="$(DESTDIR)$(qt5bindir)/qmake" \
+	--libdir-name="$(patsubst $(prefix)/%,%,$(libdir))" \
+	--disable-all \
+	--enable-opengl
+
+post-install-mythtv-version:
+	@install -d $(DESTDIR)$(versiondir) 
+	@echo "branch $(MYTHTV_GARVERSION_SHORT) through GIT $(MYTHTV_SVN_VERSION)" > $(DESTDIR)$(versiondir)/$(GARNAME)
+	@$(MAKECOOKIE)
