5f780fb0f959c4c98898928274bcdea6ae706772
Piotr Oniszczuk <warpme@o2.pl>
2019-01-29 12:35:57 +0100

    Add myth-test for testing purpouses

---
+++ script/myth-test/mythtv/package-api.mk
@@ -0,0 +1,26 @@
+MYTHTV_GARVERSION_SHORT = test
+MYTHTV_SVN_VERSION = 20190129-gac0410e
+MYTHTV_GIT_VERSION = v31-Pre-130-gac0410e
+
+MYTHTV_VERSION = $(MYTHTV_GARVERSION_SHORT)-$(MYTHTV_SVN_VERSION)
+
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_CONFIGURE_ENV = \
+	PYTHONXCPREFIX=$(DESTDIR)$(prefix)
+MYTHTV_BUILD_ENV     =
+MYTHTV_INSTALL_ENV   = \
+	INSTALL_ROOT="/" \
+
+MYTHTV_PLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(DESTDIR)$(prefix)" \
+	--sysroot="$(DESTDIR)$(rootdir)" \
+	--qmake="$(DESTDIR)$(qt5bindir)/qmake" \
+	--libdir-name="$(patsubst $(prefix)/%,%,$(libdir))" \
+	--disable-all \
+	--enable-opengl
+
+post-install-mythtv-version:
+	@install -d $(DESTDIR)$(versiondir) 
+	@echo "branch $(MYTHTV_GARVERSION_SHORT) through GIT $(MYTHTV_SVN_VERSION)" > $(DESTDIR)$(versiondir)/$(GARNAME)
+	@$(MAKECOOKIE)
