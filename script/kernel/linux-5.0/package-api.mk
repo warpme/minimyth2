9655334a7cc9e53248e1a7be9aefb25139f369db
Piotr Oniszczuk <warpme@o2.pl>
2019-02-25 14:33:50 +0100

    Fix kernel 5.0 build

--- script/kernel/linux-5.0/package-api.mk
+++ script/kernel/linux-5.0/package-api.mk
@@ -1,9 +1,9 @@
 
 #--Use this for mainline x.y.z kernel-------
 LINUX_MAJOR_VERSION = 5
-LINUX_MINOR_VERSION = 0-rc7
+LINUX_MINOR_VERSION = 0
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = 
+LINUX_EXTRA_VERSION = -rc7
 #-------------------------------------------
 
 #--Use this for mainline git-commit kernel--
