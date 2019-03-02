1c3208abbcd0d1511a90fe02ae855db18d7ab6cf
Piotr Oniszczuk <warpme@o2.pl>
2019-03-02 16:13:25 +0100

    Update mainline kernel 5.0 to 5.0-rc8; update lima kernel patches

--- script/kernel/linux-5.0/package-api.mk
+++ script/kernel/linux-5.0/package-api.mk
@@ -3,7 +3,7 @@
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 0
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc7
+LINUX_EXTRA_VERSION = -rc8
 #-------------------------------------------
 
 #--Use this for mainline git-commit kernel--
