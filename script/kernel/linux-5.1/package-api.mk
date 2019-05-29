78ae8455da87d54e385d0cd304dd2813e95932cc
Piotr Oniszczuk <warpme@o2.pl>
2019-05-29 20:10:58 +0200

    Update mainline 5.1 kernel to 5.1.5

--- script/kernel/linux-5.1/package-api.mk
+++ script/kernel/linux-5.1/package-api.mk
@@ -1,7 +1,7 @@
 #--Use this for mainline x.y.z kernel-------
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 1
-LINUX_TEENY_VERSION = 3
+LINUX_TEENY_VERSION = 5
 LINUX_EXTRA_VERSION =
 #-------------------------------------------
 
