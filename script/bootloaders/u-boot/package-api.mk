d86cfa44f41e239202699c3beaef7efc3c1bc4aa
Piotr Oniszczuk <warpme@o2.pl>
2019-07-04 16:32:30 +0200

    Move u-boot ver to package-api.mk; update u-boot to current master

---
+++ script/bootloaders/u-boot/package-api.mk
@@ -0,0 +1,6 @@
+GARNAME      = u-boot-20190703
+GARVERSION   = ca4491f2d2af93d8b77c48c5d272d88094fc06eb
+CATEGORIES   = kernel
+MASTER_SITES = https://github.com/u-boot/u-boot/archive/
+DISTFILES    = $(GARVERSION).zip
+WORKSRC      = $(WORKDIR)/u-boot-$(GARVERSION)
