7230031db6e5dd955473536af63cfda435d269e4
Piotr Oniszczuk <warpme@o2.pl>
2019-09-23 16:42:24 +0200

    Add board-rpi3_32b in bootloader & u-boot; update u-boot to current git

--- script/bootloaders/u-boot/package-api.mk
+++ script/bootloaders/u-boot/package-api.mk
@@ -1,5 +1,5 @@
 GARNAME      = u-boot-20190703
-GARVERSION   = ca4491f2d2af93d8b77c48c5d272d88094fc06eb
+GARVERSION   = 780a17e814503f71d1b51d578b32bc9c89933183
 CATEGORIES   = kernel
 MASTER_SITES = https://github.com/u-boot/u-boot/archive/
 DISTFILES    = $(GARVERSION).zip
