0469d3d15d3110d5ee4e4a41941c1df62d527f35
Piotr Oniszczuk <warpme@o2.pl>
2019-12-20 18:39:05 +0100

    Update u-boot to g643366b

--- script/bootloaders/u-boot/package-api.mk
+++ script/bootloaders/u-boot/package-api.mk
@@ -1,5 +1,5 @@
-GARNAME      = u-boot-20190703
-GARVERSION   = 780a17e814503f71d1b51d578b32bc9c89933183
+GARNAME      = u-boot-20191220
+GARVERSION   = 643366bcd5e32878a951e39b8b553b794695b026
 CATEGORIES   = kernel
 MASTER_SITES = https://github.com/u-boot/u-boot/archive/
 DISTFILES    = $(GARVERSION).zip
