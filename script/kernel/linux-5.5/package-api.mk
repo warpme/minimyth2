db5622d8c1aea7c9fc691a929a88046b593be032
Piotr Oniszczuk <warpme@o2.pl>
2020-02-18 11:22:26 +0100

    Update 5.5 mainline kernel to 5.5.4

--- script/kernel/linux-5.5/package-api.mk
+++ script/kernel/linux-5.5/package-api.mk
@@ -2,50 +2,10 @@
 ifeq (1,1)
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 5
-LINUX_TEENY_VERSION = 3
+LINUX_TEENY_VERSION = 4
 LINUX_EXTRA_VERSION = 
 endif
 #-------------------------------------------
-
-#--Use this for RC mainline kernel----------
-ifeq (0,1)
-LINUX_MAJOR_VERSION = 5
-LINUX_MINOR_VERSION = 5
-LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc7
-endif
-#-------------------------------------------
-
-#--Use this for mainline git-commit kernel--
-ifeq (0,1)
-GITHASH             = 84df9525b0c27f3ebc2ebb1864fa62a97fdedb7d
-LINUX_MAJOR_VERSION = 4
-LINUX_MINOR_VERSION = 20
-LINUX_TEENY_VERSION = 0
-LINUX_EXTRA_VERSION = 
-endif
-#-------------------------------------------
-
-#--Use this for amlogic media-tree kernel---
-ifeq (0,1)
-GITHASH             = a9e4990fa2c2b7defc47a0053371e5691d80fcdb
-LINUX_MAJOR_VERSION = 5
-LINUX_MINOR_VERSION = 4
-LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc8
-endif
-#-------------------------------------------
-
-#--Use this for Chewitt amlogic kernel------
-ifeq (0,1)
-GITHASH             = 25f7644a10234313b7040d98e668a94e87532ddf
-LINUX_MAJOR_VERSION = 5
-LINUX_MINOR_VERSION = 5
-LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc1
-endif
-#-------------------------------------------
-
 
 LINUX_VERSION      = $(LINUX_MAJOR_VERSION).$(LINUX_MINOR_VERSION)$(if $(LINUX_TEENY_VERSION),.$(LINUX_TEENY_VERSION))$(LINUX_EXTRA_VERSION)
 LINUX_FULL_VERSION = $(LINUX_MAJOR_VERSION).$(LINUX_MINOR_VERSION)$(if $(LINUX_TEENY_VERSION),.$(LINUX_TEENY_VERSION),.0)$(LINUX_EXTRA_VERSION)
