d59f6fdf3104d15bbf1bed6a97f337188dc4c46c
Piotr Oniszczuk <warpme@o2.pl>
2020-02-23 20:46:35 +0100

    Add board-rk3399.rockpi4-b; rename board-rk3328 to board-rk3328.beelink_a1

--- script/bootloaders/u-boot/package-api.mk
+++ script/bootloaders/u-boot/package-api.mk
@@ -1,5 +1,5 @@
-GARNAME      = u-boot-20191220
-GARVERSION   = 643366bcd5e32878a951e39b8b553b794695b026
+GARNAME      = u-boot-20200220
+GARVERSION   = 8e51bf746a11d7f67416859da73a83109af4e0a3
 CATEGORIES   = kernel
 MASTER_SITES = https://github.com/u-boot/u-boot/archive/
 DISTFILES    = $(GARVERSION).zip
