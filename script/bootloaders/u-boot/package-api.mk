a90e68e1c62dda2f4b8b883416b2e42e22078d69
Piotr Oniszczuk <warpme@o2.pl>
2020-03-17 19:27:59 +0100

    Rework bootloaders

--- script/bootloaders/u-boot/package-api.mk
+++
@@ -1,6 +0,0 @@
-GARNAME      = u-boot-20200220
-GARVERSION   = 8e51bf746a11d7f67416859da73a83109af4e0a3
-CATEGORIES   = kernel
-MASTER_SITES = https://github.com/u-boot/u-boot/archive/
-DISTFILES    = $(GARVERSION).zip
-WORKSRC      = $(WORKDIR)/u-boot-$(GARVERSION)
