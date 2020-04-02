671be8eb1ad2cefeee68dbc33b4116dff061d437
Piotr Oniszczuk <warpme@o2.pl>
2020-04-02 17:04:18 +0200

    Switch gstreamer back to automake building as meson built gstreamer not works OK

--- script/gstreamer/meta/package-api.mk
+++ script/gstreamer/meta/package-api.mk
@@ -2,8 +2,8 @@
 GST_VERSION      = 1.16.2
 
 GST_MASTER_SITES = \
-	$(foreach                                                                      \
-		dir,                                                                   \
-		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gstreamer, \
-	        http://gstreamer.freedesktop.org/src/$(dir)/                           \
+	$(foreach                                                                                       \
+		dir,                                                                                    \
+		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer, \
+	        http://gstreamer.freedesktop.org/src/$(dir)/                                            \
 	)
