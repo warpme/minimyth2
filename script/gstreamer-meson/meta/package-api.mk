e2006087ebf457ae0461af732ff59ea9c35c5ebd
Piotr Oniszczuk <warpme@o2.pl>
2020-04-02 17:05:11 +0200

    Switch gstreamer meson build scrits for future work

---
+++ script/gstreamer-meson/meta/package-api.mk
@@ -0,0 +1,9 @@
+
+GST_VERSION      = 1.16.2
+
+GST_MASTER_SITES = \
+	$(foreach                                                                      \
+		dir,                                                                   \
+		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gstreamer, \
+	        http://gstreamer.freedesktop.org/src/$(dir)/                           \
+	)
