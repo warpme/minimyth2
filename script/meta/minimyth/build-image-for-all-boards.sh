da3bd2eaec522c6d593be0ad6ccb4d51b8fd14ed
Piotr Oniszczuk <warpme@o2.pl>
2020-05-25 17:53:31 +0200

    Add rpi3/rpi4 mainline building in convienience script

--- script/meta/minimyth/build-image-for-all-boards.sh
+++ script/meta/minimyth/build-image-for-all-boards.sh
@@ -3,7 +3,8 @@ mm_conf_file="${HOME}/.minimyth2/minimyth.conf.mk"
 mm_home=`grep "^mm_HOME " ${mm_conf_file} | sed -e 's/.*\?=*\s//'`
 cd ${mm_home}/script/meta/minimyth
 
-# make reinstall-new-board mm_BOARD_TYPE="board-sm1"
+make reinstall-new-board mm_BOARD_TYPE="board-rpi3.mainline64"
+make reinstall-new-board mm_BOARD_TYPE="board-rpi4.mainline64"
 make reinstall-new-board mm_BOARD_TYPE="board-sm1 board-h6.beelink_gs1"
 make reinstall-new-board mm_BOARD_TYPE="board-s905 board-h6.eachlink_mini"
 make reinstall-new-board mm_BOARD_TYPE="board-rk3328.beelink_a1"
