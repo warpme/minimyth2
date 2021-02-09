ebffd37f92e9eb2b66338743087e1dbb29b7013c
Piotr Oniszczuk <warpme@o2.pl>
2021-02-09 09:58:59 +0100

    Update mythtv-master to current GIT (r2235)

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -16,7 +16,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk  ,      $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 11.13.3.r2223
+mm_VERSION_MINIMYTH ?= 11.13.3.r2235
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
