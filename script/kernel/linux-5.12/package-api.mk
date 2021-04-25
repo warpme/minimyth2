ba2d9e846453bfd88de7ab4f476d7f2b89f6f7c6
Piotr Oniszczuk <warpme@o2.pl>
2021-04-25 19:23:59 +0200

    Update 5.12 kernel patches

--- script/kernel/linux-5.12/package-api.mk
+++ script/kernel/linux-5.12/package-api.mk
@@ -21,7 +21,7 @@ ifeq (1,1)
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 12
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc3
+LINUX_EXTRA_VERSION = -rc8
 endif
 #-------------------------------------------
 
