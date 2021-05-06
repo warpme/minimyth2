29f25e6a999c8c705796b15046e4001621cdf7a6
Piotr Oniszczuk <warpme@o2.pl>
2021-05-06 21:45:12 +0200

    mythtv-master: update to current git master (r2750)

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -16,7 +16,7 @@ mm_VERSION_MYTH           ?= $(strip \
                                 $(if $(filter trunk  ,      $(mm_MYTH_VERSION)),trunk.$(mm_MYTH_TRUNK_VERSION)) \
                               )
 
-mm_VERSION_MINIMYTH ?= 11.19.0.r2749
+mm_VERSION_MINIMYTH ?= 11.19.0.r2750
 
 mm_VERSION_EXTRA          ?= $(strip \
                                 $(if $(filter yes,$(mm_DEBUG)),-debug) \
