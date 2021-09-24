434266a5522552be0cef35ae86d7e281a16f0048
Piotr Oniszczuk <piotr.oniszczuk@gmail.pl>
2021-09-24 13:13:05 +0200

    mythtv-29: add skiping python/perl libs checks. code from 98d34f4080

--- script/myth-29/mythtv/package-api.mk
+++ script/myth-29/mythtv/package-api.mk
@@ -1,6 +1,7 @@
 MYTHTV_GARVERSION_SHORT = 29
 MYTHTV_SVN_VERSION = 20200212-g4c432df
 MYTHTV_GIT_VERSION = v29.1-67-g4c432df
+MYTHTV_GIT_HASH    = 4c432df737d5d45c836f7c29f416b621d1740aca
 
 MYTHTV_VERSION = $(MYTHTV_GARVERSION_SHORT)-$(MYTHTV_SVN_VERSION)
 
@@ -15,10 +16,11 @@ MYTHTV_INSTALL_ENV   = \
 MYTHTV_PLUGINS_CONFIGURE_ARGS = \
 	--prefix="$(DESTDIR)$(prefix)" \
 	--sysroot="$(DESTDIR)$(rootdir)" \
-	--qmake="$(DESTDIR)$(qt5bindir)/qmake" \
+	--qmake="$(qtqmake)" \
 	--libdir-name="$(patsubst $(prefix)/%,%,$(libdir))" \
 	--disable-all \
-	--enable-opengl
+	--enable-opengl \
+	--enable-cross-compile \
 
 post-install-mythtv-version:
 	@install -d $(DESTDIR)$(versiondir) 
