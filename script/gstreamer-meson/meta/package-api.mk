87bbce4395f52957278f857af5016770a5edc014
Piotr Oniszczuk <piotr.oniszczuk@gmail.pl>
2021-09-30 19:29:59 +0200

    gstreamer: switch build system to Meson; update all components to 1.19.2 (except gst-libav)
    gst-libav is build with old Automake as I can't find
    way to build it on Meson with build-in FFmpeg.
    Building with build-in FFmpeg and exact 1.16.2 version of gst-libav
    is only working solution to get HTML video playback working ok in QtWebKIT.
    gst-liav build against external FFmpeg gives still picture in HTML5
    video playback in QtWebKIT

--- script/gstreamer-meson/meta/package-api.mk
+++
@@ -1,9 +0,0 @@
-
-GST_VERSION      = 1.18.0
-
-GST_MASTER_SITES = \
-	$(foreach                                                                      \
-		dir,                                                                   \
-		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gstreamer, \
-	        http://gstreamer.freedesktop.org/src/$(dir)/                           \
-	)
--- script/gstreamer/meta/package-api.mk
+++ script/gstreamer/meta/package-api.mk
@@ -1,9 +1,9 @@
 
-GST_VERSION      = 1.16.2
+GST_VERSION      = 1.19.2
 
 GST_MASTER_SITES = \
-	$(foreach                                                                                       \
-		dir,                                                                                    \
-		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer, \
-	        http://gstreamer.freedesktop.org/src/$(dir)/                                            \
+	$(foreach                                                                      \
+		dir,                                                                   \
+		gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gstreamer, \
+	        http://gstreamer.freedesktop.org/src/$(dir)/                           \
 	)
