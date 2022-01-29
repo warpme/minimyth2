21ccbb48b5e9eacca2d85937ff5c5cc49073661a
Piotr Oniszczuk <piotr.oniszczuk@gmail.pl>
2022-01-29 19:48:53 +0100

    kernel: add vp9 support on hantro g2; update 5.16 kernel to 5.16.4

--- script/kernel/linux-5.16/package-api.mk
+++ script/kernel/linux-5.16/package-api.mk
@@ -2,7 +2,7 @@
 ifeq (1,1)
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 16
-LINUX_TEENY_VERSION = 3
+LINUX_TEENY_VERSION = 4
 LINUX_EXTRA_VERSION = 
 endif
 #-------------------------------------------
