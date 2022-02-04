926c283168a2df259de14db5a1e63eff94ed6024
Piotr Oniszczuk <piotr.oniszczuk@gmail.pl>
2022-02-04 11:24:25 +0100

    myth-master: update to current git (r3521)

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       11.27.0.r3493
+mm_VERSION_MINIMYTH ?=       11.27.0.r3521
 
 # The version of Myth to use.
 # Valid values are '29', '30', '31', '32', 'master' and 'test'
