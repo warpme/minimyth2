e48cdd63fe1bbd1dc6956d94f4f6f9c7162e46d3
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-04-05 09:07:23 +0200

    mm2: bump mm2 to v12.0.0

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       11.29.0.r12
+mm_VERSION_MINIMYTH ?=       12.0.0.r16
 
 # The version of Myth to use.
 # Valid values are '29', '30', '31', '32', 'master' and 'test'
