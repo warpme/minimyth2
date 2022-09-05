fcae7206c4d3a85fbf85999a95c049d1f8d85fa4
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-09-05 12:54:10 +0200

    kernel: 5.19: update to 5.19.7

--- script/kernel/linux-5.19/package-api.mk
+++ script/kernel/linux-5.19/package-api.mk
@@ -2,7 +2,7 @@
 ifeq (1,1)
 LINUX_MAJOR_VERSION = 5
 LINUX_MINOR_VERSION = 19
-LINUX_TEENY_VERSION = 5
+LINUX_TEENY_VERSION = 7
 LINUX_EXTRA_VERSION = 
 endif
 #-------------------------------------------
