aa56f87d05fb45e0ba5ae8bd979299b68645d968
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-09-28 14:36:51 +0200

    kernel: 6.0: update to 6.0-rc7

--- script/kernel/linux-6.0/package-api.mk
+++ script/kernel/linux-6.0/package-api.mk
@@ -17,16 +17,16 @@ endif
 #-------------------------------------------
 
 #--Use this for RC mainline kernel----------
-ifeq (0,1)
+ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 0
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc5
+LINUX_EXTRA_VERSION = -rc7
 endif
 #-------------------------------------------
 
 #--Use this for RC git hash snapshot of mainline kernel----------
-ifeq (1,1)
+ifeq (0,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 0
 LINUX_TEENY_VERSION = 
