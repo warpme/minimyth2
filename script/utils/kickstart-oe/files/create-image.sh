warpme/minimyth2

commit 845cd16188360b89be498f645d43b95d46e758b0

kickstart-oe: make default sd card gpt layout without loader/uboot/env partitions and add x96-x6 variant with those partitions

script/utils/kickstart-oe/checksums
@@ -1,3 +1,80 @@
+a5f125ba527f4dce02e37b01eed73bf8  download/backport-001-wic-Add--offset-argument-for-partitions.patch
+f7c5e2b5b6067c0d5f0fa113d7bbd565  download/backport-002-wic-Fix--extra-space-argument-handling.patch
+4df608acb856b8127213a71b74987a66  download/backport-003-wic-Fix-error-message-when-reporting-invalid-offset.patch
+130285531d48cbe1f948d8ab06d278fd  download/backport-004-wic-Add-512-Byte-alignment-to--offset.patch
+667621e4b045f2a0dda406518883a242  download/board-g12.radxa_zero.conf
+f11bbec0b7390d160e06e1fd99cc95a7  download/board-g12.radxa_zero.wks
+33c2ca20ebb7bf4a3391fb7a54356e5f  download/board-h616.orangepi_zero2.conf
+418cb3929ed27d15c867624ff4b083a8  download/board-h616.orangepi_zero2.wks
+33c2ca20ebb7bf4a3391fb7a54356e5f  download/board-h616.t95.conf
+418cb3929ed27d15c867624ff4b083a8  download/board-h616.t95.wks
+33c2ca20ebb7bf4a3391fb7a54356e5f  download/board-h616.tanix_tx6s.conf
+418cb3929ed27d15c867624ff4b083a8  download/board-h616.tanix_tx6s.wks
+33c2ca20ebb7bf4a3391fb7a54356e5f  download/board-h616.x96_mate.conf
+418cb3929ed27d15c867624ff4b083a8  download/board-h616.x96_mate.wks
+239ac917d7044b100724919c12f36a8c  download/board-h6.beelink_gs1.conf
+1b2fd8d36905000c6187b0154582ac81  download/board-h6.beelink_gs1.wks
+239ac917d7044b100724919c12f36a8c  download/board-h6.eachlink_mini.conf
+1b2fd8d36905000c6187b0154582ac81  download/board-h6.eachlink_mini.wks
+239ac917d7044b100724919c12f36a8c  download/board-h6.orangepi_3.conf
+1b2fd8d36905000c6187b0154582ac81  download/board-h6.orangepi_3.wks
+239ac917d7044b100724919c12f36a8c  download/board-h6.tanix_tx6.conf
+239ac917d7044b100724919c12f36a8c  download/board-h6.tanix_tx6_mini.conf
+1b2fd8d36905000c6187b0154582ac81  download/board-h6.tanix_tx6_mini.wks
+1b2fd8d36905000c6187b0154582ac81  download/board-h6.tanix_tx6.wks
+be88f1130e7d695df96e89ca1eaa474f  download/board-rk3328.beelink_a1.conf
+ae7a25cfa92c294be7e1b8727e4e4d07  download/board-rk3328.beelink_a1.wks
+92ef5c418e16070b0fe75a31275b8ac1  download/board-rk3399.orangepi_4.conf
+fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.orangepi_4.wks
+92ef5c418e16070b0fe75a31275b8ac1  download/board-rk3399.rockpi4-b.conf
+fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.rockpi4-b.wks
+7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.quartz64.conf
+1682940e5b32bca0428cff745c6dc860  download/board-rk3566.quartz64.wks
+7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.x96_x6.conf
+1682940e5b32bca0428cff745c6dc860  download/board-rk3566.x96_x6.wks
+627d61592547b7ca7b3c1e85311d447c  download/board-rk3568.rock3-a.conf
+2e362fe9957bd31bc6dfccfff20ef0d8  download/board-rk3568.rock3-a.wks
+627d61592547b7ca7b3c1e85311d447c  download/board-rk3568.rock3-b.conf
+2e362fe9957bd31bc6dfccfff20ef0d8  download/board-rk3568.rock3-b.wks
+2c8ad46e7dedb7134f207d4e9be9b65e  download/board-rpi2.conf
+8569afc0195d1030fb7c698b4eeff82e  download/board-rpi2.wks
+2438644b28477f68ec846dea7aa27c0c  download/board-rpi34.mainline64.conf
+dc83d2cac91755be744a1f04b0d4626a  download/board-rpi34.mainline64.wks
+7195277ee76f8c85547893e0ef45553e  download/board-rpi3.mainline32.conf
+47d0b222832348ec6d03185bad22f260  download/board-rpi3.mainline32.wks
+b8725442a7953c723eb9d3d398d7c59e  download/board-rpi3.mainline64.conf
+ba2f9d1816c3aaa853cb7d706434d0da  download/board-rpi3.mainline64.wks
+e811282721e5473c29158ba68fca3a34  download/board-rpi3.rpi32.conf
+47d0b222832348ec6d03185bad22f260  download/board-rpi3.rpi32.wks
+87c311985fa7699edbd2aeef8f150f8a  download/board-rpi4.mainline64.conf
+e365720ad84c36c89169a9f773163b9a  download/board-rpi4.mainline64.wks
+45857632781c2a7cabe6d3d492f6f195  download/board-rpi4.rpi32.conf
+0a110e6365685749324631b4d8ee0b1a  download/board-rpi4.rpi32.wks
+c1f4711c9f6ba70cac7010fb8b99a718  download/board-rpi4.rpi64.conf
+f30bbcc901f1fea964cf59aabe8a0806  download/board-rpi4.rpi64.wks
+cca4b0ea38fda2846bd9c029c976cb6a  download/board-s905.conf
+b3f87d810a52990561eb92ae3078539e  download/board-s905.wks
+db2e5c571912280cdbc500ef2a5e3c41  download/board-s912.conf
+9f0110bbb7e93b11218a2991566e08e1  download/board-s912.wks
+745c6b1816a5dddd9d6386ba41bbceff  download/board-sm1.tanix_tx5_plus.conf
+25e7a49e4f68adba438c683d68fa5002  download/board-sm1.tanix_tx5_plus.wks
+bbc45e68e54865045384dff7b20f1d0f  download/board-sm1.x96_air2g.conf
+b9a52ead0215ad111c6bf142dc6a5dbd  download/board-sm1.x96_air2g.wks
+079c17cefba053d0e954b130fc37e076  download/board-x86pc.bios.conf
+079c17cefba053d0e954b130fc37e076  download/board-x86pc.bios_efi64.conf
+c5e11dbdd0c49ffaad05a60b67dbcf95  download/board-x86pc.bios_efi64.wks
+93bcdf99e2b3af249c0c0c3c25592b5d  download/board-x86pc.bios.wks
+079c17cefba053d0e954b130fc37e076  download/board-x86pc.efi64.conf
+4dcc34270a8da0aa703552231151007a  download/board-x86pc.efi64.wks
+e63ffdc75dc831590b88e503838fb96c  download/create-image.sh
+e5904ee9d725731edfc06924c1b593d2  download/default-gpt.wks
+3236c8dcd1b84a915f2e58b44d768fbd  download/default-mbr.wks
+00c064ee7fa3cb05ad5b1b2407e698c6  download/poky-yocto-2.7.3-add-biosplusefi-image-plugin.patch
+29c4ec430f5274c1ea68ce8acc28dd80  download/poky-yocto-2.7.3-add-copy-mm2-files-into-image-boot-part.patch
+3de1bf5613b66e28a47ac9f89e91d0a0  download/poky-yocto-2.7.3-allow-arbitral-part-start-sector.patch
+af69ee45810760fb1f4fa97a921ea555  download/poky-yocto-2.7.3-bitbake-skip-python2-checks.patch
+9e5b56904cdb512f921e846edffe91e6  download/poky-yocto-2.7.3-make-PARTUUID-as-constant-0x346d1a6a.patch
+ce658c77a29952e8df57938146ca80fd  download/rk3566.x96_x6-gpt.wks
 f7f73750e6a7e5861bb3d2bc8df64cf3  download/poky-yocto-2.7.3.tar.bz2
 2c8ad46e7dedb7134f207d4e9be9b65e  download/board-rpi2.conf
 8569afc0195d1030fb7c698b4eeff82e  download/board-rpi2.wks
@@ -64,14 +141,6 @@ fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.orangepi_4.wks
 079c17cefba053d0e954b130fc37e076  download/board-x86pc.bios_efi64.conf
 c5e11dbdd0c49ffaad05a60b67dbcf95  download/board-x86pc.bios_efi64.wks
 3236c8dcd1b84a915f2e58b44d768fbd  download/default-mbr.wks
-ce658c77a29952e8df57938146ca80fd  download/default-gpt.wks
-684cdac16d5d5a8dbdb89f03798c270e  download/create-image.sh
-af69ee45810760fb1f4fa97a921ea555  download/poky-yocto-2.7.3-bitbake-skip-python2-checks.patch
-00c064ee7fa3cb05ad5b1b2407e698c6  download/poky-yocto-2.7.3-add-biosplusefi-image-plugin.patch
-29c4ec430f5274c1ea68ce8acc28dd80  download/poky-yocto-2.7.3-add-copy-mm2-files-into-image-boot-part.patch
-9e5b56904cdb512f921e846edffe91e6  download/poky-yocto-2.7.3-make-PARTUUID-as-constant-0x346d1a6a.patch
-a5f125ba527f4dce02e37b01eed73bf8  download/backport-001-wic-Add--offset-argument-for-partitions.patch
-f7c5e2b5b6067c0d5f0fa113d7bbd565  download/backport-002-wic-Fix--extra-space-argument-handling.patch
-4df608acb856b8127213a71b74987a66  download/backport-003-wic-Fix-error-message-when-reporting-invalid-offset.patch
-130285531d48cbe1f948d8ab06d278fd  download/backport-004-wic-Add-512-Byte-alignment-to--offset.patch
-3de1bf5613b66e28a47ac9f89e91d0a0  download/poky-yocto-2.7.3-allow-arbitral-part-start-sector.patch
+ce658c77a29952e8df57938146ca80fd  download/rk3566.x96_x6-gpt.wks
+e5904ee9d725731edfc06924c1b593d2  download/default-gpt.wks
+e63ffdc75dc831590b88e503838fb96c  download/create-image.sh

script/utils/kickstart-oe/files/create-image.sh
@@ -87,8 +87,11 @@ echo "#----Entries to create boot & rootfs partitions" >> ${base_dir}/MiniMyth2.
 # Selecting appropriate common.wks file
 if [ ! -z `echo ${boards} | grep -o "board-x86pc"` ] ; then
     echo "  board-x86pc detected. skipping default-mbr[gpt].wks"
+elif [ ! -z `echo ${boards} | grep -o "board-rk3566.x96_x6"` ] ; then
+    echo "  board-rk35xx detected: using rk3566.x96_x6-gpt.wks"
+    cat ${base_dir}/rk3566.x96_x6-gpt.wks >> ${base_dir}/MiniMyth2.wks
 elif [ ! -z `echo ${boards} | grep -o "board-rk3566.*"` ] ; then
-    echo "  board-rk35xx detected. Using default-gpt.wks"
+    echo "  board-rk35xx detected: using default-gpt.wks"
     cat ${base_dir}/default-gpt.wks >> ${base_dir}/MiniMyth2.wks
 else
     echo "  board-x86pc detected. Using default-mbr.wks"

script/utils/kickstart-oe/Makefile
@@ -70,6 +70,7 @@ DISTFILES    = \
 	board-x86pc.bios_efi64.conf \
 	board-x86pc.bios_efi64.wks  \
 	default-mbr.wks             \
+	rk3566.x96_x6-gpt.wks       \
 	default-gpt.wks             \
 	create-image.sh             \
 

script/utils/kickstart-oe/files/default-gpt.wks
@@ -1,7 +1,9 @@
 
-part /loader --ondisk mmcblk --label loader --offset 64s    --size 7M
-part /uboot  --ondisk mmcblk --label uboot  --offset 16384s --size 7M
-part /env    --ondisk mmcblk --label env    --offset 32768s --size 15M
+# commented as generic rk3566 ipl not needs partitions for: loader, uboot and env.
+# raw copy of: idbloader and uboot.img is enough. Known exception is: rk3566 based x96_x6 TV Box
+# part /loader --ondisk mmcblk --label loader --offset 64s    --size 7M
+# part /uboot  --ondisk mmcblk --label uboot  --offset 16384s --size 7M
+# part /env    --ondisk mmcblk --label env    --offset 32768s --size 15M
 part /efi    --source bootimg-partition  --ondisk mmcblk --fstype=vfat --label BOOT --active --offset 65536s --size 512
 part /       --source rootfs             --ondisk mmcblk --fstype=ext4 --label ROOT          --align 1024    --extra-space 512
 bootloader --ptable gpt

script/utils/kickstart-oe/files/rk3566.x96_x6-gpt.wks
@@ -1,0 +1,7 @@
+
+part /loader --ondisk mmcblk --label loader --offset 64s    --size 7M
+part /uboot  --ondisk mmcblk --label uboot  --offset 16384s --size 7M
+part /env    --ondisk mmcblk --label env    --offset 32768s --size 15M
+part /efi    --source bootimg-partition  --ondisk mmcblk --fstype=vfat --label BOOT --active --offset 65536s --size 512
+part /       --source rootfs             --ondisk mmcblk --fstype=ext4 --label ROOT          --align 1024    --extra-space 512
+bootloader --ptable gpt

commit 0dae29a293ac7ea32ecd2b1b84ac1cf8cb5ae06f

bootloaders: x96-x6: fix broken bootloader for this board introduced by 845cd161

script/utils/kickstart-oe/checksums
@@ -31,7 +31,7 @@ fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.rockpi4-b.wks
 7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.quartz64.conf
 1682940e5b32bca0428cff745c6dc860  download/board-rk3566.quartz64.wks
 7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.x96_x6.conf
-1682940e5b32bca0428cff745c6dc860  download/board-rk3566.x96_x6.wks
+dbe5ab3011c0f1d57cb92387dd5e4d78  download/board-rk3566.x96_x6.wks
 627d61592547b7ca7b3c1e85311d447c  download/board-rk3568.rock3-a.conf
 2e362fe9957bd31bc6dfccfff20ef0d8  download/board-rk3568.rock3-a.wks
 627d61592547b7ca7b3c1e85311d447c  download/board-rk3568.rock3-b.conf
@@ -66,7 +66,7 @@ c5e11dbdd0c49ffaad05a60b67dbcf95  download/board-x86pc.bios_efi64.wks
 93bcdf99e2b3af249c0c0c3c25592b5d  download/board-x86pc.bios.wks
 079c17cefba053d0e954b130fc37e076  download/board-x86pc.efi64.conf
 4dcc34270a8da0aa703552231151007a  download/board-x86pc.efi64.wks
-e63ffdc75dc831590b88e503838fb96c  download/create-image.sh
+2437a632af0a444612deee88ed581799  download/create-image.sh
 e5904ee9d725731edfc06924c1b593d2  download/default-gpt.wks
 3236c8dcd1b84a915f2e58b44d768fbd  download/default-mbr.wks
 00c064ee7fa3cb05ad5b1b2407e698c6  download/poky-yocto-2.7.3-add-biosplusefi-image-plugin.patch
@@ -109,7 +109,7 @@ fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.rockpi4-b.wks
 92ef5c418e16070b0fe75a31275b8ac1  download/board-rk3399.orangepi_4.conf
 fee666b2e4e2a877c080a745c10f4089  download/board-rk3399.orangepi_4.wks
 7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.x96_x6.conf
-1682940e5b32bca0428cff745c6dc860  download/board-rk3566.x96_x6.wks
+dbe5ab3011c0f1d57cb92387dd5e4d78  download/board-rk3566.x96_x6.wks
 7328233f7077dda1bd1a00ef102a131b  download/board-rk3566.quartz64.conf
 1682940e5b32bca0428cff745c6dc860  download/board-rk3566.quartz64.wks
 627d61592547b7ca7b3c1e85311d447c  download/board-rk3568.rock3-a.conf
@@ -143,4 +143,4 @@ c5e11dbdd0c49ffaad05a60b67dbcf95  download/board-x86pc.bios_efi64.wks
 3236c8dcd1b84a915f2e58b44d768fbd  download/default-mbr.wks
 ce658c77a29952e8df57938146ca80fd  download/rk3566.x96_x6-gpt.wks
 e5904ee9d725731edfc06924c1b593d2  download/default-gpt.wks
-e63ffdc75dc831590b88e503838fb96c  download/create-image.sh
+2437a632af0a444612deee88ed581799  download/create-image.sh

script/utils/kickstart-oe/files/board-rk3566.x96_x6.wks
@@ -2,3 +2,10 @@
 part /loader --source rawcopy --sourceparams="file=@MM_HOME@/images/main/boot/rk3566loaderfiles/idblock.bin" --ondisk mmcblk --no-table --align 32
 part /uboot  --source rawcopy --sourceparams="file=@MM_HOME@/images/main/boot/rk3566loaderfiles/uboot.img"   --ondisk mmcblk --no-table --align 8192
 
+
+part /loader --ondisk mmcblk --label loader --offset 64s    --size 7M
+part /uboot  --ondisk mmcblk --label uboot  --offset 16384s --size 7M
+part /env    --ondisk mmcblk --label env    --offset 32768s --size 15M
+part /efi    --source bootimg-partition  --ondisk mmcblk --fstype=vfat --label BOOT --active --offset 65536s --size 512
+part /       --source rootfs             --ondisk mmcblk --fstype=ext4 --label ROOT          --align 1024    --extra-space 512
+bootloader --ptable gpt

script/utils/kickstart-oe/files/create-image.sh
@@ -88,8 +88,12 @@ echo "#----Entries to create boot & rootfs partitions" >> ${base_dir}/MiniMyth2.
 if [ ! -z `echo ${boards} | grep -o "board-x86pc"` ] ; then
     echo "  board-x86pc detected. skipping default-mbr[gpt].wks"
 elif [ ! -z `echo ${boards} | grep -o "board-rk3566.x96_x6"` ] ; then
-    echo "  board-rk35xx detected: using rk3566.x96_x6-gpt.wks"
-    cat ${base_dir}/rk3566.x96_x6-gpt.wks >> ${base_dir}/MiniMyth2.wks
+    echo "  board-rk35xx detected: using all in one board-rk3566.x96_x6.wks"
+    # cat board-rk3566.x96_x6.wks to MiniMyth2.wks is comented-out as x96_x6
+    # box is single exception where board-rk3566.x96_x6.wks
+    # creates all paritions (boot related and rootfs). This is because this
+    # box speciffics.
+    #cat ${base_dir}/board-rk3566.x96_x6.wks >> ${base_dir}/MiniMyth2.wks
 elif [ ! -z `echo ${boards} | grep -o "board-rk3566.*"` ] ; then
     echo "  board-rk35xx detected: using default-gpt.wks"
     cat ${base_dir}/default-gpt.wks >> ${base_dir}/MiniMyth2.wks