ca71bcbb357aba430859e81c9c311713eead6107
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-10-17 14:46:50 +0200

    kodi: v20: add v20aplha3

--- script/mediaplayers/kodi/package-api.mk
+++ script/mediaplayers/kodi/package-api.mk
@@ -1,3 +1,4 @@
 
-KODI_VERSION      = 19.4-Matrix
-KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/refs/tags/
+# v20 alpha3 9.09.2022
+KODI_VERSION      = 8d5ffc55b6688a9320905a052999b510f77d2b3a
+KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/
