5e3806d94d514ac3e945d886e846dcd8d93951c1
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-10-24 16:40:55 +0200

    mm2: add 6.1 kernel support

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -224,9 +224,9 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "5.18"          ] && \
-	    [ ! "$(mm_KERNEL_VERSION)" = "5.19"          ] && \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "5.19"          ] && \
 	    [ ! "$(mm_KERNEL_VERSION)" = "6.0"           ] && \
+	    [ ! "$(mm_KERNEL_VERSION)" = "6.1"           ] && \
 	    [ ! "$(mm_KERNEL_VERSION)" = "rpi-5.12"      ] ; then \
 		echo " " ; \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
