04f18417be1c904245e1e23229843564ce602324
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-10-31 10:03:14 +0100

    kernel: 6.1: update to 6.1-rc3

--- script/kernel/linux-6.1/package-api.mk
+++ script/kernel/linux-6.1/package-api.mk
@@ -21,7 +21,7 @@ ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 1
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc2
+LINUX_EXTRA_VERSION = -rc3
 endif
 #-------------------------------------------
 
