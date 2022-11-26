544184eb4862234b86e9da8885e423d1f832810f
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2022-11-26 13:42:59 +0100

    xorg: remove unnedded dependencies; add extra url for sources

--- script/xorg/xorg/package-api.mk
+++ script/xorg/xorg/package-api.mk
@@ -1,5 +1,6 @@
 XORG_VERSION      = 7.3
 XORG_MASTER_SITES = \
+	https://xorg.freedesktop.org/releases/individual/lib/ \
 	$(foreach                                                               \
 		dir,                                                            \
 		app data doc driver everything font lib proto xcb util xserver, \
