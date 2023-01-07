a12dbfabe3a7efbd1ee76ab66b6470ed7f8ce603
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2023-01-07 21:30:10 +0100

    minimyth.conf.mk: update git rel

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH       ?= 12.10.2.r1039
+mm_VERSION_MINIMYTH       ?= 12.10.2.r1061
 
 # The version of Myth to use.
 # Valid values are '29', '30', '31', '32', 'master' and 'test'
