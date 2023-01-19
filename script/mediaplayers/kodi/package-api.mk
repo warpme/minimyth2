e4cf0392154e2b30ac7d99889d1bd2a780444cb9
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2023-01-19 11:52:05 +0100

    kodi: switch to use ffmpeg5 by using ffmpeg5-nexus branch

--- script/mediaplayers/kodi/package-api.mk
+++ script/mediaplayers/kodi/package-api.mk
@@ -1,4 +1,4 @@
 
-# v20 alpha3 9.09.2022
-KODI_VERSION      = 8d5ffc55b6688a9320905a052999b510f77d2b3a
+# v20 Final, 15.01.2023
+KODI_VERSION      = 389e701cb96e5543c303c4004811b51684d67dd4
 KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/
