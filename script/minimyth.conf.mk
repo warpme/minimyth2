697b7bdcd2d429101ab7dccff7446ad63bf50e46
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2023-10-17 11:23:10 +0200

    minimyth.conf.mk: add orangepi4-lts

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -78,6 +78,7 @@ mm_SOFTWARE               ?= \
 # 'board-rk3399.rockpi4-b'
 # 'board-rk3399.rockpi4-se'
 # 'board-rk3399.orangepi_4'
+# 'board-rk3399.orangepi_4_lts'
 # 'board-rk3528.vontar_r3'
 # 'board-rk3566.x96_x6'
 # 'board-rk3566.quartz64-b'
