25dbca6150f3c6f2f642714a3685a3ca0d342a58
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2023-11-08 15:44:44 +0100

    kernel: 6.6: update to 6.6.1

--- script/kernel/linux-6.6/package-api.mk
+++ script/kernel/linux-6.6/package-api.mk
@@ -1,5 +1,5 @@
 #--Use this for mainline x.y.z kernel-------
-ifeq (0,1)
+ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 6
 LINUX_TEENY_VERSION = 1
@@ -8,7 +8,7 @@ endif
 #-------------------------------------------
 
 #--Use this for first release of mainline kernel
-ifeq (1,1)
+ifeq (0,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 6
 LINUX_TEENY_VERSION = 
