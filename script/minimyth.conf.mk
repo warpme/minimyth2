3d39bff18b190797ee304a070417963a30bdda83
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2023-12-22 12:23:21 +0100

    myth-master: update to current head (r610)

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       12.21.1.r596
+mm_VERSION_MINIMYTH ?=       12.22.0.r610
 
 # The version of Myth to use.
 # Valid values are '32', '33', 'master' and 'test'
