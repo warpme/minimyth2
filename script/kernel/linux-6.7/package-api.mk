84d3e429b3e32d257d290d1081b789880eb17526
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-01-02 10:40:43 +0100

    kernel: 6.7: add aic8800 initial driver; add proper aliases for eth0 in zero3w/e

--- script/kernel/linux-6.7/package-api.mk
+++ script/kernel/linux-6.7/package-api.mk
@@ -21,7 +21,7 @@ ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 7
 LINUX_TEENY_VERSION = 
-LINUX_EXTRA_VERSION = -rc7
+LINUX_EXTRA_VERSION = -rc8
 endif
 #-------------------------------------------
 
