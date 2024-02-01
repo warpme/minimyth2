09b310438a0b828c131eff63c8719c9593d810c1
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-02-01 19:25:15 +0100

    kernel: 6.7: update to 6.7.3

--- script/kernel/linux-6.7/package-api.mk
+++ script/kernel/linux-6.7/package-api.mk
@@ -2,7 +2,7 @@
 ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 7
-LINUX_TEENY_VERSION = 2
+LINUX_TEENY_VERSION = 3
 LINUX_EXTRA_VERSION = 
 endif
 #-------------------------------------------
