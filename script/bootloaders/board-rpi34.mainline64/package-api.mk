c458020424c7f6f5bf5ecd40d28f8032a1e86772
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-02-20 12:18:22 +0100

    board-rpi34.mainline64: update rpi firmware to 15.02.2024

--- script/bootloaders/board-rpi34.mainline64/package-api.mk
+++ script/bootloaders/board-rpi34.mainline64/package-api.mk
@@ -1,4 +1,4 @@
 
-RPI_GITHASH      = 42e091c8e7edb79b5a6b493daaea2485d621d9e5
-RPI_GARVERSION   = 2022-10-21
+RPI_GITHASH      = f01fa5fa89d9628d15d148c545b078f8ec19c643
+RPI_GARVERSION   = 2024-02-15
 RPI_MASTER_SITES = https://github.com/raspberrypi/firmware/raw/$(RPI_GITHASH)/boot/
