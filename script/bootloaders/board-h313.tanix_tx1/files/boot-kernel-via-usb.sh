37a8c2f66f5a130c3d3b3f22cb92841c272253ce
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-04-19 16:21:18 +0200

    board-h313.tanix_tx1: add basic install procedure. As ums not works correctly - we boot kernel then install to emmc

---
+++ script/bootloaders/board-h313.tanix_tx1/files/boot-kernel-via-usb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+welcome_string="H616"
+
+echo "Awaiting for device ..."
+
+while true
+do
+    device=$(sunxi-fel -l 2>/dev/null | sed -e 's/\s*//g')
+    if [[ "${device}" =~ "${welcome_string}" ]] ; then
+        echo "Device discovered. Good!!!"
+        break
+    else
+        sleep 1
+        echo "Awaiting for device ..."
+    fi
+done
+
+echo "Starting upload U-Boot, Linux Kernel, DTB to device ..."
+
+sunxi-fel -v -p uboot u-boot-sunxi-with-spl.bin \
+write 0x40200000 Image \
+write 0x4fa00000 dtbs/allwinner/sun50i-h313-tanix-tx1.dtb \
+write 0x4fc00000 load-kernel.scr \
+
+echo "Booting Linux kernel on device ..."
