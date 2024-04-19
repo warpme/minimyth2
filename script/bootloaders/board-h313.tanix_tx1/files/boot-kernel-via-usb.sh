ec1aeed3e6cd16da1df037436133605151fb1876
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-04-19 21:19:31 +0200

    board-h313.tanix_tx1: improve verbosity in boot-kernel script

--- script/bootloaders/board-h313.tanix_tx1/files/boot-kernel-via-usb.sh
+++ script/bootloaders/board-h313.tanix_tx1/files/boot-kernel-via-usb.sh
@@ -23,4 +23,9 @@ write 0x40200000 Image \
 write 0x4fa00000 dtbs/allwinner/sun50i-h313-tanix-tx1.dtb \
 write 0x4fc00000 load-kernel.scr \
 
-echo "Booting Linux kernel on device ..."
+echo "Booting Linux kernel on device. Please wait ..."
+
+sleep 15
+
+echo "Now you can (re)connect USB key with rootfs ..."
+echo " "
