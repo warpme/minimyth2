f8f459c8a714268e2d04c007522d55ee322af8ca
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-06-15 21:38:35 +0200

    mm2: allow use 6.10 kernel

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -235,8 +235,8 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "6.8"           ] && \
-	    [ ! "$(mm_KERNEL_VERSION)" = "6.9"           ] && \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "6.9"           ] && \
+	    [ ! "$(mm_KERNEL_VERSION)" = "6.10"           ] && \
 	    [ ! "$(mm_KERNEL_VERSION)" = "rpi-5.12"      ] ; then \
 		echo " " ; \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
