6c3d35e35b12cc9267b8b2d89fed1eef9c2e3d3d
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-08-15 20:06:22 +0200

    kernel: 6.10: update to 6.10.5

--- script/kernel/linux-6.10/package-api.mk
+++ script/kernel/linux-6.10/package-api.mk
@@ -2,7 +2,7 @@
 ifeq (1,1)
 LINUX_MAJOR_VERSION = 6
 LINUX_MINOR_VERSION = 10
-LINUX_TEENY_VERSION = 4
+LINUX_TEENY_VERSION = 5
 LINUX_EXTRA_VERSION = 
 endif
 #-------------------------------------------
