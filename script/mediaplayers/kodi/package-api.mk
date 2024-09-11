f6591c1aa720748b5cdd1d2c6608b38d6ed8fad3
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-09-11 14:05:10 +0200

    kodi: update to 21.0; stop using pack-relative-relocs as kodi uses gold linker

--- script/mediaplayers/kodi/package-api.mk
+++ script/mediaplayers/kodi/package-api.mk
@@ -1,4 +1,4 @@
 
-# v21.0, 4.06.2024
-KODI_VERSION      = 60c450005472e6d38929bb28c07d00589102818d
+# v21.1, 17.08.2024
+KODI_VERSION      = 183eb85f108c3d8b44a578de80f575f27afe9b9a
 KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/
