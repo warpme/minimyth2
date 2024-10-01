ed8f4231af7792508450ce15a7cace140fb2fac5
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-10-01 14:37:11 +0200

    board-h313.tanix_tx1[_v1.2]: stop filtering sunxi-fel stderr to show i.e. error when sctipt is launched with non-root user

--- script/bootloaders/board-h313.tanix_tx1/files/boot-kernel-via-usb.sh
+++ script/bootloaders/board-h313.tanix_tx1/files/boot-kernel-via-usb.sh
@@ -6,7 +6,7 @@ echo "Awaiting for device ..."
 
 while true
 do
-    device=$(sunxi-fel -l 2>/dev/null | sed -e 's/\s*//g')
+    device=$(sunxi-fel -l | sed -e 's/\s*//g')
     if [[ "${device}" =~ "${welcome_string}" ]] ; then
         echo "Device discovered. Good!!!"
         break
@@ -18,7 +18,7 @@ done
 
 echo "Starting upload U-Boot, Linux Kernel, DTB to device ..."
 
-sunxi-fel -v -p uboot u-boot-sunxi-with-spl.bin \
+sunxi-fel -v -p uboot bootloader/u-boot-sunxi-with-spl.bin \
 write 0x40200000 Image \
 write 0x4fa00000 dtbs/allwinner/sun50i-h313-tanix-tx1.dtb \
 write 0x4fc00000 load-kernel.scr \
@@ -27,5 +27,8 @@ echo "Booting Linux kernel on device. Please wait ..."
 
 sleep 15
 
+echo " "
+echo " "
 echo "Now you can (re)connect USB key with rootfs ..."
+echo " "
 echo " "
