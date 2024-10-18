f465f84262b6e340f0e3dcc41db495695ef964de
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-10-18 17:27:20 +0200

    minimyth.conf: udate rev; doc: update chnagelog

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       13.7.0.r329
+mm_VERSION_MINIMYTH ?=       13.7.1.r343
 
 # The version of Myth to use.
 # Valid values are '32', '33', '34', 'master' and 'test'
