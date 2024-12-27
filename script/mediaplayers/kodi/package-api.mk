87e30342170597b3e28d94032a4fb38812e56c3e
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2024-12-27 17:30:15 +0100

    kodi: update to current head (gceefd7e1e)

--- script/mediaplayers/kodi/package-api.mk
+++ script/mediaplayers/kodi/package-api.mk
@@ -1,4 +1,4 @@
 
-# master, 11.11.2024
-KODI_VERSION      = f8e7be1d00eaceb66451f39adc067e75b6265435
+# master, 27.12.2024
+KODI_VERSION      = ceefd7e1e1762a9b0fb4ba627f66a23235567cc0
 KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/
