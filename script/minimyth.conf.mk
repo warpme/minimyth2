60da02f36eabeb69439e1a38bb46084c50554fb7
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2025-03-17 14:15:32 +0100

    update rev. and changelog

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       14.1.0.r138
+mm_VERSION_MINIMYTH ?=       14.1.0.r156
 
 # The version of Myth to use.
 # Valid values are '33', '34', '35', 'master' and 'test'
