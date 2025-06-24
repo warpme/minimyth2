9befce59aefc2b73f2f750cdabcc8ff4e55f11fa
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2025-06-24 17:15:42 +0200

    kodi: update to gcb75fb5fc36

--- script/mediaplayers/kodi/package-api.mk
+++ script/mediaplayers/kodi/package-api.mk
@@ -1,4 +1,4 @@
 
-# master, 15.03.2025 v22.0-alpha1
-KODI_VERSION      = 703a14ea1d5fcb27dd706083b46404ce5a45e305
+# master, 24.06.2025 v22.0-alpha1
+KODI_VERSION      = cb75fb5fc36078ea1c86e5596e2298f2b5e4721e
 KODI_MASTER_SITES = https://github.com/xbmc/xbmc/archive/
