0015558530735f085ac2e8b3ae5f6103aaee26aa
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2025-09-06 13:28:34 +0200

    minimyth.conf.mk: bump mm2 ver to 14.4.1

--- script/minimyth.conf.mk
+++ script/minimyth.conf.mk
@@ -9,7 +9,7 @@
 mm_HOME                   ?= /home/minimyth/minimyth2
 
 # The version of MiniMyth2.
-mm_VERSION_MINIMYTH ?=       14.4.0.r518
+mm_VERSION_MINIMYTH ?=       14.4.1.r518
 
 # The version of Myth to use.
 # Valid values are '33', '34', '35', 'master' and 'test'
