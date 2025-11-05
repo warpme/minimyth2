d0c0e4b263f20c50fe51e53d3a13f11e7cbe2f2e
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2025-11-05 10:45:30 +0100

    update minimyth.conf and changelog

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -242,9 +242,9 @@ mm-all:
 		fi ; \
 	done
 	@echo "    mm_KERNEL_VERSION"
-	@if [ ! "$(mm_KERNEL_VERSION)" = "6.16"           ] && \
-	    [ ! "$(mm_KERNEL_VERSION)" = "6.17"           ] && \
+	@if [ ! "$(mm_KERNEL_VERSION)" = "6.17"           ] && \
 	    [ ! "$(mm_KERNEL_VERSION)" = "6.18"           ] && \
+	    [ ! "$(mm_KERNEL_VERSION)" = "6.19"           ] && \
 	    [ ! "$(mm_KERNEL_VERSION)" = "rpi-5.12"      ] ; then \
 		echo " " ; \
 		echo "error: mm_KERNEL_VERSION=\"$(mm_KERNEL_VERSION)\" is an invalid value." ; \
