692e8f713eb52b0cf30b966eb27bbceb610b0754
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2025-12-28 12:15:01 +0100

    mpv: switch to current head; add launch scripts for drm/x11/wayland

---
+++ script/mediaplayers/mpv/files/mpv-v4l2_request-x11.sh
@@ -0,0 +1,8 @@
+
+# --gpu-hwdec-interop=drmprime-overlay directly presents the frames using an hardware overlay.
+# --gpu-hwdec-interop=drmprime instead uses the linux facilities (dmabuf) to transfer the
+#   decoded frames from the hardware decoder buffer to the EGL/OpenGL (or whatever) presentation layer
+
+# echo "0xFF" > /sys/module/drm/parameters/debug
+
+mpv --hwdec=v4l2request --vo=gpu --gpu-context=x11egl --msg-level=vd=v,vo=v $1
