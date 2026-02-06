0849a796e4accdf3c96b8b64a60e7c9e1a27aadf
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2026-02-06 17:04:21 +0100

    myth-36: add initial ver

---
+++ script/myth-36/mythtv/package-api.mk
@@ -0,0 +1,28 @@
+
+MYTHTV_GARVERSION_SHORT = 36
+MYTHTV_SVN_VERSION      = 20260206-gb0e48fb52af
+MYTHTV_GIT_VERSION      = v36-1-gb0e48fb52af
+MYTHTV_GIT_HASH         = b0e48fb52af3a29a4d12b44edb26a8e4451cabbc
+
+MYTHTV_VERSION = $(MYTHTV_GARVERSION_SHORT)-$(MYTHTV_SVN_VERSION)
+
+MYTHTV_SOURCEDIR = $(sourcedir)/mythtv
+
+MYTHTV_CONFIGURE_ENV = \
+	PYTHONXCPREFIX=$(DESTDIR)$(prefix)
+MYTHTV_BUILD_ENV     =
+MYTHTV_INSTALL_ENV   = \
+	INSTALL_ROOT="/" \
+
+MYTHTV_PLUGINS_CONFIGURE_ARGS = \
+	--prefix="$(DESTDIR)$(prefix)" \
+	--sysroot="$(DESTDIR)$(rootdir)" \
+	--qmake="$(qtqmake)" \
+	--libdir-name="$(patsubst $(prefix)/%,%,$(libdir))" \
+	--disable-all \
+	--enable-cross-compile \
+
+post-install-mythtv-version:
+	@install -d $(DESTDIR)$(versiondir) 
+	@echo "branch $(MYTHTV_GARVERSION_SHORT) through GIT $(MYTHTV_SVN_VERSION)" > $(DESTDIR)$(versiondir)/$(GARNAME)
+	@$(MAKECOOKIE)
