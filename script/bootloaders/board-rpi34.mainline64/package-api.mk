151d60336809922f5034971e60c56bd32c55b228
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2026-02-11 14:47:45 +0100

    bootloaders: rpi3/4/5: switch all boards to rpi bootloader defined in board-rpi345.mainline64/package-api.mk; update rpi bootloader to current head

--- script/bootloaders/board-rpi34.mainline64/package-api.mk
+++
@@ -1,4 +0,0 @@
-
-RPI_GITHASH      = 063bcab6c8a90efb0d19f69d88cbbc7ec79cab68
-RPI_GARVERSION   = 2025-12-08
-RPI_MASTER_SITES = https://github.com/raspberrypi/firmware/raw/$(RPI_GITHASH)/boot/
--- script/bootloaders/board-rpi345.mainline64/package-api.mk
+++ script/bootloaders/board-rpi345.mainline64/package-api.mk
@@ -1,4 +1,4 @@
 
-RPI_GITHASH      = 063bcab6c8a90efb0d19f69d88cbbc7ec79cab68
-RPI_GARVERSION   = 2025-12-08
+RPI_GITHASH      = d9bcdf02a74b31ba33597d69b015f9e92142d220
+RPI_GARVERSION   = 2026-02-09
 RPI_MASTER_SITES = https://github.com/raspberrypi/firmware/raw/$(RPI_GITHASH)/boot/
