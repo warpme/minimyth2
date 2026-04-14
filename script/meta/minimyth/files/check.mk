f7c635b2702c70930680cf98e320959b7ff08657
Piotr Oniszczuk <piotr.oniszczuk@gmail.com>
2026-04-14 16:19:25 +0200

    check.mk: add forgoten changes from 4dfb2cc36a

--- script/meta/minimyth/files/check.mk
+++ script/meta/minimyth/files/check.mk
@@ -172,9 +172,6 @@ mm-all:
 	@for software in $(mm_SOFTWARE) ; do \
 		if [ ! "$${software}" = "mythplugins"    ] && \
 		   [ ! "$${software}" = "mythfrontend"   ] && \
-		   [ ! "$${software}" = "mythfrontend_v36" ] && \
-		   [ ! "$${software}" = "mythfrontend_v35" ] && \
-		   [ ! "$${software}" = "mythfrontend_v34" ] && \
 		   [ ! "$${software}" = "mplayer-svn"    ] && \
 		   [ ! "$${software}" = "mythnetvision"  ] && \
 		   [ ! "$${software}" = "mythbrowser"    ] && \
